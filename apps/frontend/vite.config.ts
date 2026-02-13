af67380b5dfd29d9ef4b15063cfa7ac57a5b2aa9
Ryan-Zayne <phoenixastro97@gmail.com>
2026-02-13 14:22:43 +0100

    updates

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-import { icons } from "@/components/icons/icon-constant";
+import { icons } from "./src/components/icons/icon-constant";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss(), viteTsconfigPaths(), monicon({ icons, typesFileName: "types" })],
